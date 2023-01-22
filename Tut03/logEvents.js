4b8f46810216ae837530c48e7b1bba8f0f8152cd
kiptanui boaz <boaserem022@gmail.com>
2023-01-22 09:01:20 +0300

    added a node server

--- Tut03/logEvents.js
+++ Tut03/logEvents.js
@@ -1,26 +1,23 @@
-const {format} = require("date-fns");
-const {v4: uuid} = require("uuid");
-//file system
-const fs = require("fs");
-//files system with promises 
-const fsPromises = require("fs").promises;
-//path module
-const path = require("path");
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
 
-const logEvents = async(message)=>{
-    const dateTime = `${format(new Date,"yyyy-MM-dd \t HH:mm:ss")}`;
-    const logTime = `${dateTime} \t ${uuid()} \t ${message} \n`;
-    console.log(logTime);
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-    try{
-        if(!fs.existsSync(path.join(__dirname,"logs"))){
-            await fsPromises.mkdir(path.join(__dirname,"logs"));
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname,"logs", "eventLog.txt"), logTime)
 
-    }catch(err){
-        console.error(err);
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    } catch (err) {
+        console.log(err);
     }
 }
 
- module.exports =logEvents;
+module.exports = logEvents;
