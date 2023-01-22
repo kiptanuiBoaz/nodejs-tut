bcc14602261213af3a3e705fdcd4d239352311b5
kiptanui boaz <boaserem022@gmail.com>
2023-01-22 23:23:16 +0300

    cors

--- Tut06/server.js
+++ Tut06/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require("cors")
 const {logger,logEvents} = require("./middleware/logEvents")
 const PORT = process.env.PORT || 3500;
 
 
 //custom middlewareLogger
-app.use(logger)
+app.use(logger);
+
+//cross origin resource sharing
+const whiteList = ["http://www.google.com", "http://www.localhost:3500", "http:127.0.0.1:500"]
+const corsOptions = {
+    origin: (origin, callback) =>{
+        if(whiteList.indexOf(origin) !== -1) {
+            callback(null, true)
+        }else{
+            callback(new Error("Not allowed by cors"));
+        }
+    },
+
+    optionSuccessStatus: 200
+
+}
+app.use(cors(corsOptions));
 
 // middleware function provided by the Express.js framework that is used to parse incoming request bodies that have the Content-Type of application/x-www-form-urlencoded.
 app.use(express.urlencoded({extended: false}))
